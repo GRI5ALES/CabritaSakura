f7a545382019a16c7133617dfa4f276561d6cd77
DuqueJR <duqueruizjeronimo@gmail.com>
2025-09-27 16:30:20 -0500

    Commit

--- src/main/java/Entities/SubsitemaComercial/Carrito.java
+++ src/main/java/Entities/SubsitemaComercial/Carrito.java
@@ -1,4 +1,32 @@
 package Entities.SubsitemaComercial;
 
+import java.time.LocalDate;
+
 public class Carrito {
+    private int id;
+    private LocalDate fecha_creacion;
+
+
+    public Carrito(int id){
+        this.id = id;
+        this.fecha_creacion = LocalDate.now();
+    }
+
+    public int getId() {
+        return this.id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public LocalDate getFecha_creacion(){
+        return this.fecha_creacion;
+    }
+
+    public void anadirLineaCarrito(){
+
+    }
+
+
 }
